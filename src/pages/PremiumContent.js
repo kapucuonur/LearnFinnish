f5577ba0e282b931ddcd1220ee8dd62e4ef64bcb
Onur Kapucu <kapucuonur@hotmail.com>
2026-01-15 23:36:08 +0200

    Update Pronunciation Secrets video with correct YouTube link

--- src/pages/PremiumContent.js
+++ src/pages/PremiumContent.js
@@ -342,7 +342,7 @@ export class PremiumContentPage {
                         <h3 class="media-title">Pronunciation Secrets</h3>
                     </div>
                     <div class="media-embed">
-                        <iframe src="https://www.youtube.com/embed/M7lc1UVf-VE?si=Placeholder" title="Pronunciation Secrets" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/w70KRa3kbZM?si=vOcDC81cyxpwWr4C" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </div>
                 </div>
 
